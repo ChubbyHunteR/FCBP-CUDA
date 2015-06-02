a1d772101c347746610cc986635e383ff398e408
Luka <luka.strizic@hotmail.com>
2015-06-02 20:33:09 +0200

    Update config.h

--- config.h
+++ config.h
@@ -26,8 +26,8 @@
 #define THREADS 1
 
 /*
- * Limit on how many images may be encoded/decoded at once.
+ * Limit on how many images may be encoded/decoded at once. If images are ~200kB, set to 800 for a 2GB card, or 1600 for a 4GB card.
  */
-#define CUDA_MAX_IMG 1000
+#define CUDA_MAX_IMG 800
 
 #endif /* CONFIG_H_ */
